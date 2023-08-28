a0125f93f35420e755ef4b40c29e9ecf22c3903e
Катя <mochalova.es@mail.ru>
2023-08-28 20:30:44 +0300

    homework-2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +34,33 @@ class Item:
         """
         self.price *= self.pay_rate
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_string):
+        if len(name_string) <= 10:
+            self.__name = name_string
+        else:
+            self.__name = name_string[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
+        """
+        with open('../src/items.csv', newline='', encoding='windows-1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Item.all.append(cls(row['name'], row['price'], row['quantity']))
+
+    @staticmethod
+    def string_to_number(string):
+        """
+        Статический метод, возвращающий число из числа - строки
+        """
+        if string.isdigit():
+            return int(string)
+        else:
+            return int(string.split('.')[0])
